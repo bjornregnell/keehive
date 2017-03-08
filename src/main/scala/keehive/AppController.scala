eb5ed36aeeb658f59d20717a5d6247c7aa3fba73
bjornregnell <bjorn.regnell@cs.lth.se>
2017-03-08 21:53:38 +0100

    add Main and AppController

---
+++ src/main/scala/keehive/AppController.scala
@@ -0,0 +1,17 @@
+package keehive
+
+object AppController {
+  val welcomeBanner = s"""
+    |***********************************************
+    |***          WELLCOME TO keehive!           ***
+    |***  keehive is a terminal password manager ***
+    |***    PROTECT YOUR KEYBOARD FROM PEEKERS!  ***
+    |***           Type '?' for help             ***
+    |***        Press TAB for completion         ***
+    |***********************************************
+    """.stripMargin
+
+    def start(nonDefaultPath: String = ""): Unit  = {
+      println(welcomeBanner)
+    }
+}
---
+++ src/main/scala/keehive/Main.scala
@@ -0,0 +1,16 @@
+package keehive
+
+object Main {
+  val helpArgs: Set[String] = Set("-h", "?", "--help", "-help", "help")
+  val usageHelpMsg: String = s"usage: java -jar keehive.jar [pathToVaultDir]"
+
+  def main(args: Array[String]): Unit = args.lift(0) match {
+    case Some(arg) if helpArgs contains arg => quit(usageHelpMsg)
+    case Some(path) => AppController.start(path)
+    case _          => AppController.start()
+  }
+
+  def quit(msg: String = "Goodbye!"): Unit = { println(msg); sys.exit(0) }
+
+  def abort(errMsg: String): Unit = { println(s"Error: $errMsg"); sys.exit(1) }
+}

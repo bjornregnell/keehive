35102e08faa5c9965372219783e87abf8fbea938
bjornregnell <bjorn.regnell@cs.lth.se>
2017-03-18 20:07:29 +0100

    update version file name

--- release.sh
+++ release.sh
@@ -3,6 +3,7 @@
 echo -n "Enter new version number: "
 read vers
 
+echo "$vers" | cat >version.txt
 sed -i -- "s/version = \"[0-9]\.[0-9]\"/version = \"$vers\"/g" src/main/scala/keehive/Main.scala
 sed -i -- "s/versnum = \"[0-9]\.[0-9]\"/versnum = \"$vers\"/g" build.sbt
 sed -i -- "s/keehive-[0-9]\.[0-9]\.jar/keehive-$vers.jar/g" run.sh
